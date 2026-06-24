dc5264316c0b65089da6b16300370a8ddd5d9ca7
RealNoShit <ctwk2001@gmail.com>
2026-06-24 16:48:25 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,15 @@
 @echo off
 
+echo Closing old app if running...
+taskkill /IM "Downloads Sorter.exe" /F 2>nul
+
 echo Cleaning old build...
 rmdir /s /q build 2>nul
 rmdir /s /q dist 2>nul
+del *.spec 2>nul
 
 echo Building Downloads Sorter...
-py -m PyInstaller --windowed --name "Downloads Sorter" main.py
+py -m PyInstaller --noconfirm --windowed --name "Downloads Sorter" main.py
 
 echo.
 echo Build complete!
